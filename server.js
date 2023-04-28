c18b4f18167cd5ef56450b378aa5f8f0f3c66ce3
mahilsonhagnner <mahilson.pereira@acct.global>
2023-04-28 14:29:39 -0300

    added header

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json');
 const middleware = jsonServer.defaults()
 
+// middleware para adicionar dado no header
+server.use((req, res, next) => {
+    // verifica se o header "X-Custom-Header" existe na requisição
+    if (req.headers['x-seller-id']) {
+        // adiciona o valor do header no objeto "locals" para ser utilizado posteriormente
+        res.locals.customHeader = req.headers['x-seller-id'];
+    }
+    next();
+});
+
 const port = 8080
 
 server.use(middleware)
