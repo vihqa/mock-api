22a8240d800e78eca7a0e51f789293430fbf9f45
Kaique Ramos <113634460+kaiqueramosacct@users.noreply.github.com>
2023-06-05 10:49:36 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,3 +1,57 @@
 module.exports.data = {
-    "skus": []
+    "skus": [
+        {
+            "id": 2,
+            "quantity": 10,
+            "prices": [
+                {
+                    "skuPrice": 344.20,
+                    "measurementUnitPrice": 30.00,
+                    "unitPrice": 19.00,
+                    "skuPriceWithDiscounts": 280.80,
+                    "measurementUnitPriceWithDiscounts": 27.00,
+                    "unitPriceWithDiscounts": 18.10,
+                    "appliedDiscounts": [
+                        {
+                            "discount": {
+                                "discountType": "CEILING",
+                                "discountValueType": "PERCENTAGE",
+                                "discountValue": 10
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "id": 27,
+            "quantity": 4,
+            "prices": [
+                {
+                    "skuPrice": 356.79,
+                    "measurementUnitPrice": 150.00,
+                    "unitPrice": 70.00,
+                    "skuPriceWithDiscounts": 500.91,
+                    "measurementUnitPriceWithDiscounts": 8.99,
+                    "unitPriceWithDiscounts": 17.98,
+                    "appliedDiscounts": [
+                        {
+                            "discount": {
+                                "discountType": "VOLUME",
+                                "discountValueType": "ABSOLUTE",
+                                "discountValue": 10,
+                                "items": [
+                                    {
+                                        "skuId": 279,
+                                        "minQuantity": 1,
+                                        "maxQuantity": 5
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 };
